2971ac4628c5f70c22b078d739e74d1379d19b17
diamond Hung <diamond@dropbuy.com.tw>
2020-03-18 22:37:37 +0800

    ✨ Feat: add filters

--- src/main.js
+++ src/main.js
@@ -10,6 +10,42 @@ Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
+Vue.filter("typeColor", function(value) {
+  let color = ''
+  switch(value) {
+    case 'chi':
+      color = '#67C23A'
+      break
+    case 'jp':
+      color = '#E6A23C'
+      break
+    case 'kr':
+      color = '#F56C6C'
+      break
+    case 'tai':
+      color = '#909399'
+      break
+    case 'west':
+      color = '#109399'
+      break
+    default:
+      color = '#fff'
+      break
+  }
+  return color
+})
+
+Vue.filter("typeText", function(value) {
+  const types = {
+    kr: '韓式',
+    jp: '日式',
+    tai: '泰式',
+    chi: '中式',
+    west: '西式'
+  }
+  return types[value]
+})
+
 new Vue({
   router,
   store,

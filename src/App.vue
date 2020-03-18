923b59fe158e6f2b269cbcbeab9a7e1439d9ae67
diamond Hung <diamond@dropbuy.com.tw>
2020-03-18 22:39:54 +0800

    🎨 Style: update main layout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+      <el-header id="header">
+        <router-link to="/">
+          <span>午餐吃什麼</span>
+        </router-link>
+        <router-link class="float-btn" to="/dishes">
+          <el-link icon="el-icon-edit" :underline="false">編輯菜單</el-link>
+        </router-link>
+      </el-header>
+      <el-container>
+        <el-main>
+          <router-view/>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    gotoEdit() {
+      this.$router.push({name: 'dishes'})
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -29,4 +49,24 @@
     }
   }
 }
+.el-header, .el-footer {
+  background-color: #87C241;
+  color: #fff;
+  font-size: 1.25em;
+  text-align: center;
+  line-height: 60px;
+}
+#header {
+  .float-btn {
+    float: right;
+  }
+  a {
+    color: #fff;
+    text-decoration: none;
+    &:visited {
+      color: #fff;
+    }
+  }
+}
+
 </style>
